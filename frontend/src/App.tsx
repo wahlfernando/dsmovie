670dc52d2fdbc3769dfc6294c12331d4a5f8a563
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2022-01-11 13:51:09 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import Navbar from './components/navbar';
+//componentes do react-router-dom (Foi chamado 3 compoennetes diretamente)
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-  
- 
 }
 
 export default App;
